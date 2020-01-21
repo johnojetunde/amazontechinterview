84652b9070617426a41301ce0fddd571fcc5eaad
johnjtn <john.ojetunde@telesoftas.com>
2020-01-21 18:49:43 +0200

    feat: created a test and class for the amazon tast

---
+++ src/AmazonCompetitor.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AmazonCompetitor {
+    public List<String> topNCompetitors(Integer numCompetitors, Integer topNCompetitors, List<String> competitors, Integer numReviews, List<String> reviews) {
+        List<String> totalCompetitors = sortReviews(
+                reviewFrequency(competitors, reviews)
+        );
+
+        return totalCompetitors.subList(0, topNCompetitors);
+    }
+
+    public List<String> sortReviews(Map<String, Integer> competitors) {
+        Map<String, Integer> sorted = competitors.entrySet().parallelStream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted((value1, value2) -> Long.compare(value2.getValue(), value1.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        return new ArrayList<>(sorted.keySet());
+    }
+
+    public Map<String, Integer> reviewFrequency(List<String> competitors, List<String> reviews) {
+        Map<String, Integer> reviewFreq = new HashMap<>();
+        reviews.stream()
+                .map(review -> competitorsFound(review, competitors))
+                .forEach(competitorsFound -> updateCompetitorFrequency(competitorsFound, reviewFreq));
+
+        return reviewFreq;
+    }
+
+    private Stream<String> competitorsFound(String review, List<String> competitors) {
+        return competitors.stream()
+                .filter(s -> review.toLowerCase().contains(s.toLowerCase()));
+    }
+
+    private void updateCompetitorFrequency(Stream<String> competitors, Map<String, Integer> reviewFreq) {
+        competitors.forEach(s -> {
+            Integer currentCount = reviewFreq.getOrDefault(s, 0);
+            reviewFreq.put(s, ++currentCount);
+        });
+    }
+}
---
+++ src/AmazonCompetitorTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class AmazonCompetitorTest {
+
+    private AmazonCompetitor amazon;
+    private static Integer numCompetitors = 6;
+    private static Integer topNCompetitors = 2;
+    private static List<String> competitors = asList("newshop", "shownow", "afshion", "fashionbeats", "mymarket", "tcellular");
+    private static Integer numReviews = 6;
+    private static List<String> reviews = asList("newshop is providing good services in the city; everyone should use newshop", "best services by newshop",
+            "fashionbeats has great services in the city", "I am proud to have fashionbeats", "mymarket has awesome services", "Thanks Newshop for the quick delivery");
+
+    @BeforeEach
+    void setUp() {
+        amazon = new AmazonCompetitor();
+    }
+
+    @Test
+    public void resultShouldNotGreaterThan__topNCompetitors() {
+        List<String> topCompetitors = amazon.topNCompetitors(numCompetitors, topNCompetitors, competitors, numReviews, reviews);
+        assertFalse(topCompetitors.size() > topNCompetitors);
+    }
+
+    @Test
+    public void shouldGroupByCompetitorsCorrectly() {
+        Map<String, Integer> topCompetitors = amazon.reviewFrequency(competitors, reviews);
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("newshop", 3);
+        expected.put("fashionbeats", 2);
+        expected.put("mymarket", 1);
+
+        assertEquals(expected, topCompetitors);
+    }
+
+    @Test
+    public void shouldSortGroupedCompetitorsCorrectly() {
+        Map<String, Integer> data = new HashMap<>();
+        data.put("newshop", 3);
+        data.put("fashionbeats", 3);
+        data.put("mymarket", 1);
+
+        List<String> topCompetitors = amazon.sortReviews(data);
+        assertEquals(asList("fashionbeats", "newshop", "mymarket"), topCompetitors);
+    }
+
+    @Test
+    public void shouldGet_topNCompetitors() {
+        List<String> topCompetitors = amazon.topNCompetitors(numCompetitors, topNCompetitors, competitors, numReviews, reviews);
+        assertEquals(asList("newshop", "fashionbeats"), topCompetitors);
+    }
+
+}
