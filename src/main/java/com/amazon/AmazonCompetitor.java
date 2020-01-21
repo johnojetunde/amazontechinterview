7b576aaef335a4d58ed7a34f6249892d747393dd
johnjtn <john.ojetunde@telesoftas.com>
2020-01-21 18:54:41 +0200

    feat: project structure creation

--- src/main/java/com/amazon/AmazonCompetitor.java
+++ src/main/java/com/amazon/AmazonCompetitor.java
@@ -1,3 +1,5 @@
+package com.amazon;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
--- test/java/com/amazon/AmazonCompetitorTest.java
+++ test/java/com/amazon/AmazonCompetitorTest.java
@@ -1,3 +1,5 @@
+package com.amazon;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
